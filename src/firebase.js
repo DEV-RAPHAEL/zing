9155dae98f5e9c175ec68743eee570422bfac562
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-08-09 20:18:39 -0700

    Zinging... please wait

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBrWFQOSmPLxuMwa5jItf7UBGUw6pQD0WU",
+  authDomain: "zing-fundutech.firebaseapp.com",
+  projectId: "zing-fundutech",
+  storageBucket: "zing-fundutech.appspot.com",
+  messagingSenderId: "543417455153",
+  appId: "1:543417455153:web:7962261ba2584ace61fb10",
+  measurementId: "G-S05P7KJDX7"
+};
