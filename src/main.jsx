ae9fa1f01c243ce5f826be2e5838d4e90668f2f3
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-08-11 00:03:34 -0700

    material ui and styled components added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import App from "./App";
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />} />
+  </Routes>
+</BrowserRouter>
+);
